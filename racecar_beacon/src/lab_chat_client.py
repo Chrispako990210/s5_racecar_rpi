f96a0fb5141b005759264d4f44e7a08e223ded0d
huguesdup <hugues.dupuis@usherbrooke.ca>
2022-09-08 15:25:34 -0400

    changed credentials commit

--- racecar_beacon/src/lab_chat_client.py
+++ racecar_beacon/src/lab_chat_client.py
@@ -2,9 +2,13 @@
 
 import socket
 
-HOST = '127.0.0.1'
+HOST = '192.168.137.10' #'127.0.0.1'
 PORT = 65432
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
 
+s.send('Hello, world') # send same data
+data = s.recv(1024) # receive the response
+print(data) # print the result
+s.close() # close the connection
--- racecar_beacon/src/lab_chat_server.py
+++ racecar_beacon/src/lab_chat_server.py
@@ -2,9 +2,19 @@
 
 import socket
 
-HOST = '127.0.0.1'
+HOST = '192.168.137.10' #'127.0.0.1'
 PORT = 65432
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
 
+s.listen(1)
+(conn, addr) = s.accept() # returns new socket and addr. client
+while True: # forever
+    data = conn.recv(1024) # receive data from client
+    if not data: 
+        break # stop if client stopped
+    #conn.send(raw_input("> ")) # send data to client
+    conn.raw_input("> ") # send data to client
+    #conn.send(str(data)+"*") # return sent data plus an "*"
+conn.close() # close the connection
