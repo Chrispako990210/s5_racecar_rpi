11004a1e3aadac23e6e7bfe117ed8358bb44e113
Chrispako990210 <pacc2101@usherbrooke.ca>
2022-09-08 00:24:06 -0400

    Latest update

--- racecar_beacon/src/remote_client.py
+++ racecar_beacon/src/remote_client.py
@@ -6,3 +6,8 @@ HOST = '127.0.0.1'
 # This process should listen to a different port than the PositionBroadcast client.
 PORT = 65432
 
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as r_client:
+    r_client.connect((HOST, PORT))
+    #define message format here
+    r_client.sendall(b'Hello, world')
+    data = r_client.recv(1024)
--- racecar_beacon/src/ros_monitor.py
+++ racecar_beacon/src/ros_monitor.py
@@ -3,6 +3,7 @@
 import rospy
 import socket
 import threading
+from struct import * # Ici, je me base sur l'annexe mais c'est pas une bonne pratique de faire un import * !
 
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import LaserScan
@@ -24,13 +25,14 @@ class ROSMonitor:
 
         # Thread for RemoteRequest handling:
         self.rr_thread = threading.Thread(target=self.rr_loop)
-        
+        self.rr_thread.start()
         # Thread for PositionBroadcast handling:
         self.pb_thread = threading.Thread(target=self.pb_loop)
-
+        self.pb_thread.start()
+        rospy.on_shutdown(self.shutdown_ros_monitor) # What to do when ROS is shutting down
         print("ROSMonitor started.")
 
-    def odom_cb(self, data: Odometry):
+    def odom_cb(self, data: Odometry): # For testing, we will use the ros bag file!
         pass
 
     def rr_loop(self):
@@ -47,12 +49,14 @@ class ROSMonitor:
                     data = conn.recv(1024) # change reception size depending on format
                     if not data:
                         break
-                    conn.sendall(data)
+                    conn.send(data)
 
     def pb_loop(self):
         HOST = '10.0.1.4' # Valeur bidon, a linker plus tard
         # Init your socket here :
-        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as pb_socket: # Inits the socket with (Address family-> IPv4, Transport protocol-> UDP)
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as pb_socket: # Inits the socket with (Address family-> IPv4, Transport protocol-> UDP)\
+            rospy.sleep(1) # 1Hz broadcast rate
+            pb_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1) # Mode broadcast
             pb_socket.bind((HOST, self.pos_broadcast_port)) # Might be better to change the pos_prodcast definition with vehicule tracker
             pb_socket.listen()
             conn, addr = pb_socket.accept()
@@ -64,6 +68,11 @@ class ROSMonitor:
                     if not data:
                         break
                     conn.send(data)
+
+    def shutdown_ros_monitor(self):
+        self.rr_thread.join()
+        self.pb_thread.join()
+        # Maybe we should close the socket here ?
 
 if __name__=="__main__":
     rospy.init_node("ros_monitor")
