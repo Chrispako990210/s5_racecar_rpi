026726bcf9711af66cc16cbfef6328e94bc9a25d
huguesdup <hugues.dupuis@usherbrooke.ca>
2022-09-10 08:31:26 -0400

    Lab3 is working on 127.0.0.1

--- racecar_beacon/src/lab_chat_client.py
+++ racecar_beacon/src/lab_chat_client.py
@@ -2,13 +2,13 @@
 
 import socket
 
-HOST = '192.168.137.10' #'127.0.0.1'
+HOST = '127.0.0.1'
 PORT = 65432
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
 
-s.send('Hello, world') # send same data
+s.send("Hello, world".encode("UTF_8")) # send same data
 data = s.recv(1024) # receive the response
 print(data) # print the result
-s.close() # close the connection
+s.close() # close the connection"
--- racecar_beacon/src/lab_chat_server.py
+++ racecar_beacon/src/lab_chat_server.py
@@ -2,7 +2,7 @@
 
 import socket
 
-HOST = '192.168.137.10' #'127.0.0.1'
+HOST = '127.0.0.1'
 PORT = 65432
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -12,9 +12,8 @@ s.listen(1)
 (conn, addr) = s.accept() # returns new socket and addr. client
 while True: # forever
     data = conn.recv(1024) # receive data from client
-    if not data: 
+    if not data:
         break # stop if client stopped
-    #conn.send(raw_input("> ")) # send data to client
-    conn.raw_input("> ") # send data to client
-    #conn.send(str(data)+"*") # return sent data plus an "*"
+    print(data)
+    conn.send(data + "*".encode("UTF_8")) # return sent data plus an "*"
 conn.close() # close the connection
