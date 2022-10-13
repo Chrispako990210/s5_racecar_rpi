229199faeacf37f1e4524a369712f1b594682184
Chris Pacheco <pacc2101@usherbrooke.ca>
2022-10-13 15:18:42 -0400

    latest_update_app3

---
+++ racecar_web_interface/www/js/racecar_main.js
@@ -0,0 +1,83 @@
+      // Define some global variables
+      var rbServer = null;
+      var cmdVelTopic = null;
+
+      //Some initializations after the page has been shown
+      $(document).ready(function(){
+        // Textarea for logging
+        const textarea = document.getElementById("log");
+        textarea.scrollTop = textarea.scrollHeight;
+
+        // Buttons and event handlers
+        const conn_btn = document.getElementById("connect_button");
+        const clear_btn = document.getElementById("clear_button");
+        const up_btn = document.getElementById("");
+        const down_btn = document.getElementById("down_button");
+        const left_btn = document.getElementById("left_button");
+        const right_btn = document.getElementById("right_button");
+      });
+
+      // Define some functions
+      function connectROS() {
+        // This function connects to the rosbridge server
+        var ip_addr = document.getElementById("ip_address").value;
+        console.log(ip_addr);
+
+	    rbServer = new ROSLIB.Ros({
+          // Assuming ros server IP is 10.42.0.1
+	      url : 'ws://'+ ip_addr +':9090'
+	    });
+
+        rbServer.on('connection', function(){
+            console.log('Connected to websocket server.');
+            textarea.value += 'Connected to websocket server with address' + ip_addr;
+            // These lines create a topic object as defined by roslibjs
+            cmdVelTopic = new ROSLIB.Topic({
+                ros : rbServer,
+                name : '/racecar/cmd_vel_abtr_2',
+                messageType : 'geometry_msgs/Twist'
+            });
+
+            camTopi = new ROSLIB.Topic({
+                ros : rbServer,
+                name: '/racecar/camera/rgb/image_raw'
+            });
+	    });
+
+        rbServer.on('error', function(error) {
+          console.log('Error connecting to websocket server: ', error);
+	    });
+
+	    rbServer.on('close', function() {
+          console.log('Connection to websocket server closed.');
+	    });
+      }
+
+      function eraseStatus() {
+        document.getElementById('log').value = '';
+        console.log('Clearing log');
+      }
+
+
+      // These lines create a message that conforms to the structure of the Twist defined in our ROS installation
+      // It initalizes all properties to zero. They will be set to appropriate values before we publish this message.
+      var twist = new ROSLIB.Message({
+           linear : {
+                x : 0.0,
+                y : 0.0,
+                z : 0.0
+            },
+            angular : {
+                x : 0.0,
+                y : 0.0,
+                z : 0.0
+            }
+      });
+
+      //Publishing loop cmd_vel at 5 Hz
+      setInterval(function(){
+           if(cmdVelTopic != null)
+           {
+             cmdVelTopic.publish(twist);
+           }
+      }, 200);
